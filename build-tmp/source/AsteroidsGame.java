3bc0af43f57bb1da2c948985b9f7f0fabf638356
unknown <thwhite@697-334-Boromir.sf.k12.internal>
2014-10-31 10:11:55 -0700

    First Commit

--- build-tmp/source/AsteroidsGame.java
+++ build-tmp/source/AsteroidsGame.java
@@ -1,3 +1,19 @@
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class AsteroidsGame extends PApplet {
+
 //your variable declarations here
 //SpaceShip normandy;
 Floatable[] thingies;
@@ -235,5 +251,14 @@ public void keyPressed() {
     ((SpaceShip)thingies[0]).setPointDirection((int)(Math.random()*360));
     ((SpaceShip)thingies[0]).setDirectionX(0);
     ((SpaceShip)thingies[0]).setDirectionY(0);
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "AsteroidsGame" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
